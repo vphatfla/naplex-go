1a10c8df0de239e5cd406ecb70d6879a858517e4
Van Phan Mac Air <pvphat25@gmail.com>
2025-04-12 14:09:35 -0400

    fix piepleline writer

--- web-crawler/naplex_crawler/naplex_crawler/pipelines.py
+++ web-crawler/naplex_crawler/naplex_crawler/pipelines.py
@@ -32,6 +32,7 @@ class JsonWritePipeline:
 
     def close_spider(self, spider):
         """ Save questions when spider is closed"""
+        self.logger.info(f'close spider w len item {len(self.items)}')
         json.dump(self.items, self.json_file, indent=2, ensure_ascii=False)
         self.json_file.close()
 
@@ -39,52 +40,4 @@ class JsonWritePipeline:
         """Process logic"""
         self.items.append(dict(item))
         return item
-class NaplexCrawlerSpiderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
 
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, or item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request or item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info("Spider opened: %s" % spider.name)
-
-
-    def process_item(self, item, spider):
-        return item
