d1cd2c37267ec14b72ab19509f553f5bf144156f
Van Phan Mac Air <pvphat25@gmail.com>
2025-04-30 21:08:15 -0400

    add list retries (queue)

--- data-transform-gemini/main.go
+++ data-transform-gemini/main.go
@@ -46,10 +46,13 @@ func main() {
 		log.Fatal(err)
 	}
 	total := int(t)
+	var retriesList []int
+	var errList []int
+	var skipList []int
 
 	var wg sync.WaitGroup
 
-	for start  := 1; start <= total; start += 10{
+	for start := 1; start <= total; start += 10{
 		end := min(start + 9, total)
 		log.Printf("Retrieving raw data from %v to %v", start, end)
 		rawQuestions, err := queries.GetRawQuestionWithRange(ctx,
@@ -65,27 +68,33 @@ func main() {
 		for _,rQ := range rawQuestions {
 			if strings.Contains(rQ.Title, "Preparing for Success") {
 				log.Printf("Skipping id %v title %v", rQ.ID, rQ.Title)
+				skipList = append(skipList, int(rQ.ID))
 				continue
 			}
 			wg.Add(1)
 			go func(rQ db.RawQuestion) {
+				defer wg.Done()
+
 				log.Printf("Requesting content for raw questions id %v title %v", rQ.ID, rQ.Title)
 				m := gemini.NewModelJson(client, cfg)
 				txt, err := gemini.GetContent(ctx, m, rQ.RawQuestion)
 				if err != nil {
-					log.Printf("err -> %v", err)
+					log.Printf("id = %v --> ERROR %v", rQ.ID, err)
+					errList = append(errList, int(rQ.ID))
 					return
 				}
 
-				// log.Printf("id %v & title %v --> \n%v", rQ.ID, rQ.Title, string(txt))
 				var temp db.ProcessedQuestion
 				temp.Title = rQ.Title
 
 				err = json.Unmarshal([]byte(txt), &temp)
 				if err != nil {
-					log.Printf("raw id = %v -->  txt = %v ", rQ.ID,txt)
-					log.Panic(err)
+					log.Printf("id =%v --> ERROR %v", rQ.ID, err )
+					log.Printf("raw id = %v -->  txt = %v ", rQ.ID, txt)
+					retriesList = append(retriesList, int(rQ.ID))
+					return
 				}
+
 				log.Printf("Successfully Unmarshal for raw question id = %v", rQ.ID)
 
 				id, err := queries.InsertProcessedQuestion(ctx, db.InsertProcessedQuestionParams{
@@ -98,10 +107,11 @@ func main() {
 					Link: rQ.Link,
 				})
 				if err != nil {
-					log.Panic(err)
+					log.Printf("id = %v --> ERROR %v", rQ.ID, err)
+					retriesList = append(retriesList, int(rQ.ID))
+					return
 				}
 				log.Printf("Inserted data into processed table id %v", id)
-				wg.Done()
 				return
 			}(rQ)
 
@@ -109,9 +119,11 @@ func main() {
 		// sleep one minute since it's the free version threshold!
 		log.Println("Sleeping ...")
 		time.Sleep(1*time.Minute)
-
 	}
 
 	wg.Wait()
+
+	log.Printf("Retries list = %v", retriesList)
+	log.Printf("Error list = %v", errList)
 	return
 }
