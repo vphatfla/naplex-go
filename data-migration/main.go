3e70c6d3e474e7aaa25c2e6ac2113ce6a89d9c5d
Van Phan Mac Air <pvphat25@gmail.com>
2025-07-10 20:30:18 -0400

    example worker with sync group

--- data-migration/main.go
+++ data-migration/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
 
+func worker(jobs chan int, results chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := range jobs {
+		fmt.Printf("START: worker job %d\n", i)
+		time.Sleep(time.Second)
+		fmt.Printf("END: worker job %d\n", i)
+		results <- i*10
+	}
+}
 func main() {
 	log.Print("Data Migration")
+
+	jobs := make(chan int)
+	results := make(chan int)
+
+	var wg sync.WaitGroup
+	go func() {
+		for r := range results {
+			fmt.Printf("Result  = %d\n", r)
+		}
+	}()
+
+	for j := 1; j <= 7; j += 1 {
+		wg.Add(1)
+		go worker(jobs, results, &wg)
+	}
+
+
+	for i := 10; i <= 20; i+=1 {
+		jobs <- i
+	}
+	close(jobs)
+	wg.Wait()
 }
